a07409b4426cdbb27218bfea9a2bdbf110452cb9
JanuaryAle <aleksatokarieva@gmail.com>
2020-11-12 01:10:22 +0300

    bot and web devided

--- index.js
+++ index.js
@@ -1,41 +1,13 @@
-const Telegraf = require('telegraf')
-const {
-    Markup,
-    Extra,
-    session
-} = Telegraf
+
 const Koa = require('koa')
 const Router = require('koa-router')
 const bodyParser = require('koa-bodyparser')
 require('dotenv').config()
 
+const bot = require('./bot.js')
+
 const TOKEN = process.env.BOT_TOKEN
 const PORT = process.env.PORT
-const URL = process.env.URL
-
-// Настройка бота
-
-const bot = new Telegraf(TOKEN)
-
-bot.use(async (ctx, next) => {
-    const start = new Date()
-    await next()
-    const response_time = new Date() - start
-    console.log(`(Response Time: ${response_time})`)
-  })
-
-bot.use(session())
-//bot.use(stage.middleware())
-
-bot.telegram.setWebhook(`${URL}/bot${TOKEN}`)
-
-bot.on('message', ctx => {
-    ctx.reply('Pong')
-})
-
-bot.hears(/\/help (.+)/, (ctx, [source, match]) => {
-    ctx.reply(match)
-})
 
 // Добавляем роуты
 
@@ -44,9 +16,7 @@ const app = new Koa()
 const router = Router()
 
 router.post(`/bot${TOKEN}`, async ctx => {
-    const {body} = ctx.request
-    console.log(`${body}\n${ctx.response}`)
-    await bot.handleUpdate(ctx.request.body, ctx.response)
+    await bot.handleUpdate(ctx.request.body, ctx.response)  // Наконец-то, эта штука задана правилно и бот реагирует
     ctx.status = 200
 })
 
@@ -59,61 +29,9 @@ router.get(`/`, ctx => {    // Все ок, сервер работает
 app.use(bodyParser())
 app.use(router.routes())
 
+// Бот готов получать посты от телеги, он слушает
+
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
 })
-
-
-// const TelegramBot = require('node-telegram-bot-api')
-// const Koa = require('koa')
-// const Router = require('koa-router')
-// const bodyParser = require('koa-bodyparser')
-// require('dotenv').config()
-
-// const TOKEN = process.env.BOT_TOKEN
-// const PORT = process.env.PORT
-// const URL = process.env.URL
-
-// // Настройка бота
-
-// const bot = new TelegramBot(TOKEN)
-
-// bot.on('message', msg => {
-//     const {chat: {id}} = msg
-//     bot.sendMessage(id, 'Pong')
-// })
-
-// bot.onText(/\/help (.+)/, (msg, [source, match]) => {
-//     const {chat: {id}} = msg
-//     bot.sendMessage(id, match)
-// })
-
-// bot.setWebHook(`${URL}/bot${TOKEN}`)
-
-// // Добавляем роуты
-
-// const app = new Koa()
-
-// const router = Router()
-
-// router.post(`/bot${TOKEN}`, ctx => {
-//     const {body} = ctx.request
-//     console.log(body)
-//     bot.processUpdate(body)
-//     ctx.status = 200
-// })
-
-// router.get(`/`, ctx => {    // Все ок, сервер работает
-//     ctx.status = 200
-// })
-
-// // Ставим мидлы
-
-// app.use(bodyParser())
-// app.use(router.routes())
-
-// app.listen(PORT, () => {
-//     console.log(`Listening on ${PORT}`)
-// })
-
 
