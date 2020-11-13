163a069be9c0b0bf54c7a6c5c471d29e0f63d51f
JanuaryAle <aleksatokarieva@gmail.com>
2020-11-13 18:58:31 +0300

    c

--- bot.js
+++ bot.js
@@ -36,7 +36,6 @@ bot.use(async (ctx, next) => {
     await next()
     const response_time = new Date() - start
     console.log(`(Response Time: ${response_time})`)
-    console.log(`(Response Time: ${ctx})`)
   })
 
 bot.use(session())
