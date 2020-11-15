00d18e2c78102829c1ad982afaeeebf11db0f323
JanuaryAle <aleksatokarieva@gmail.com>
2020-11-15 16:40:24 +0300

    c

--- admin/fondsScene.js
+++ admin/fondsScene.js
@@ -43,7 +43,7 @@ async function beginMessage(ctx){
                 ctx.webhookReply = true
             }
     }
-    await ctx.wizard.selectStep(1)
+    return await ctx.wizard.selectStep(1)
 }
 
 class FondSceneGenerator{
